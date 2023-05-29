1ab4f1dd4dd7234f3251aa0a93c0d3dc53a2883a
Oreva <khemical51@gmail.com>
2023-05-29 13:09:28 +0100

    feat: add eraser and clear button functionality

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,16 +1,38 @@
-const container = document.getElementById('container')
+const rangeInput = document.getElementById('input');
+const colorInput = document.getElementById('color-picker');
+const randomMode = document.getElementById('random-color')
+const eraser = document.getElementById('eraser')
+const clear = document.getElementById('clear');
+const defaultInput = rangeInput.value;
+const defaultColor = 'rgb(0, 0, 0)';
+const container = document.getElementById('container');
+console.log(defaultInput);
+console.log(colorInput.value);
 
-function createSquares() {
-    for (let i = 0; i < 256; i++) {
-        const div = document.createElement('div')
-        div.classList.add('boxes')
-        container.appendChild(div)
+
+
+
+function createSquares(grid) {
+    const gridSize = grid * grid;
+    for (let i = 0; i < gridSize; i++) {
+        const div = document.createElement('div');
+        div.classList.add('boxes');
+        container.style.gridTemplateColumns = `repeat(${grid}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${grid}, 1fr)`;
+        container.appendChild(div);
     }
-    return
 }
 
-createSquares()
+createSquares(defaultInput);
 
+
+// Clear button
+clear.addEventListener('click', e => {
+    container.innerHTML = '';
+})
+
+
+//Set random color picker
 function randomColor() {
     let color = [];
     for (let i = 0; i < 3; i++) {
@@ -21,9 +43,45 @@ function randomColor() {
 
 console.log(randomColor())
 
-container.addEventListener('click', e => {
-    if (e.target && e.target.matches('.boxes')) {
-        e.target.style.backgroundColor = randomColor()
-    }
-})
+// Change background color of squares
+function addEvent(colors) {
+    container.addEventListener('mousedown', e => {
+        if (colors) {
+            if (e.target && e.target.matches('.boxes')) {
+                e.target.style.backgroundColor = colors
+            }
+        } else {
+            if (e.target && e.target.matches('.boxes')) {
+                e.target.style.backgroundColor = randomColor()
+            }
+        }
+    }) 
+}
 
+
+window.addEventListener('load', startup, false);
+function startup() {
+  addEvent(colorInput.value)  
+
+  colorInput.addEventListener('input', e => {
+    addEvent(e.target.value)
+  })
+
+  eraser.addEventListener('click', e => {
+    e.target.backgroundColor = '#FFFFFF'
+    addEvent(e.target.backgroundColor)
+  })
+
+  randomMode.addEventListener('click', e => {
+    e.target.value = ''
+    console.log(e.target.value)
+    addEvent(e.target.value)
+  })
+}
+
+//Clear container and set squares to input value
+rangeInput.addEventListener('input', e => {
+    let newInput = parseInt(e.target.value)
+    container.innerHTML = ''
+    createSquares(newInput)
+})
