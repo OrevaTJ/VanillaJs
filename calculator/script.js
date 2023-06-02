13b444591a1af5279dc4a2f5eae147e002d3e2d1
Oreva <khemical51@gmail.com>
2023-06-02 22:03:25 +0100

    get more than one number input

--- calculator/script.js
+++ calculator/script.js
@@ -1,17 +1,28 @@
 let display = document.getElementById('input');
-let buttons = Array.from(document.querySelectorAll('#num-btn'))
-const addButton = document.getElementById('add')
-const equalButton = document.getElementById('equal')
-const multButton = document.getElementById('multiply')
-const clearButton = document.getElementById('clear')
+let buttons = Array.from(document.querySelectorAll('#num-btn'));
+const addButton = document.getElementById('add');
+const equalButton = document.getElementById('equal');
+const multButton = document.getElementById('multiply');
+const clearButton = document.getElementById('clear');
+const divideButton = document.getElementById('divide');
+const subButton = document.getElementById('sub');
+const opButton = Array.from(document.querySelectorAll('#operators'))
 
 let valueA = [];
+operator = '';
 
+//Operator functions
 function add() {
     return valueA[0] + valueA[1]
 }
 function multiply() {
     return valueA[0] * valueA[1]
+}
+function divide() {
+    return valueA[0] / valueA[1]
+}
+function sub() {
+    return valueA[0] - valueA[1]
 }
 
 function equal(op) {
@@ -27,21 +38,24 @@ clearButton.addEventListener('click', () => {
     clear()
 })
 
-operator = ''
-addButton.addEventListener('click', () => {
-    operator = '+'
-    console.log(operator)
-})
-multButton.addEventListener('click', () => {
-    operator = '*'
-    console.log(operator)
-})
+//Event for each operator buttons
+opButton.forEach(op => op.addEventListener('click', e => {
+    operator = e.target.textContent
+    world()
+}))
 
+
+//Event for each numbers
 buttons.forEach(btn => btn.addEventListener('click', e => {
-    display.value = e.target.textContent
-    valueA.push(parseInt(e.target.textContent))
+    display.value += e.target.textContent
+    // valueA.push(parseInt(e.target.textContent))
     console.log(valueA)
 }))
+
+function world() {
+    valueA.push(parseInt(display.value))
+    console.log(valueA)
+}
 
 equalButton.addEventListener('click', () => {
     if (operator === '+') {
@@ -51,6 +65,14 @@ equalButton.addEventListener('click', () => {
     } else if (operator === '*') {
         equal(multiply)
         display.value = multiply()
+        clear()
+    } else if (operator === '/') {
+        equal(divide)
+        display.value = divide()
+        clear()
+    } else if (operator === '-') {
+        equal(sub)
+        display.value = sub()
         clear()
     }
 })
