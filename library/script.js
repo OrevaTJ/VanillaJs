562eba882962fc52bcff4b4a1294a06493bd274d
Oreva <khemical51@gmail.com>
2023-06-25 23:35:07 +0100

    .

--- library/script.js
+++ library/script.js
@@ -1,5 +1,6 @@
 const formElement = document.querySelector('form');
-const bookList = document.getElementById('book-list')
+const bookList = document.getElementById('book-list');
+
 
 let myLibrary = [];
 let list = [];
@@ -14,26 +15,56 @@ function addLibrary(add) {
     myLibrary.push(add)
 }
 
+
+function createCheckbox(title, author, pages) {
+    const booksDiv = document.createElement('div');
+    const booksTitle = document.createElement('p');
+    const booksAuthor = document.createElement('p');
+    const booksPage = document.createElement('p');
+    const infoButton = document.createElement('button');
+    const removeButton = document.createElement('button');
+
+    booksDiv.classList.add('books-div');
+
+    booksTitle.textContent = title;
+    booksAuthor.textContent = author;
+    booksPage.textContent = pages;
+
+    booksDiv.appendChild(booksTitle);
+    booksDiv.appendChild(booksAuthor);
+    booksDiv.appendChild(booksPage);
+    booksDiv.appendChild(infoButton);
+    booksDiv.appendChild(removeButton);
+
+    bookList.appendChild(booksDiv)
+}
+
 function display() {
     let myList = '';
+    let box = createCheckbox()
     for (let x in myLibrary[myLibrary.length - 1]) {
         myList += myLibrary[myLibrary.length - 1][x] + '<br>';
     }
-    list.push(myList + '<br>')
+    list.push(myList)
+    list.push()
     let join = list.join(' ')
     bookList.innerHTML = join
+
+    //createCheckbox()
 }
 
 formElement.addEventListener('submit', e => {
     let bookTitle =  e.target.elements.title.value;
     let bookAuthor = e.target.elements.author.value;
     let bookPages = e.target.elements.pages.value;
+
+    // createCheckbox(bookTitle, bookAuthor, bookPages)
     
 
-    let newBook = new Book(bookTitle, bookAuthor, bookPages);
+    // let newBook = new Book(bookTitle, bookAuthor, bookPages);
 
-    addLibrary(newBook)
-    display()
+    // addLibrary(newBook)
+    // display()
 
     e.preventDefault();
 }, false)
--- tic-tac-to/script.js
+++ tic-tac-to/script.js
@@ -1,11 +1,14 @@
 const container = document.getElementById('container');
 
+
 let gameBoard = ['box1', 'box2', 'box3', 'box4', 
             'box5', 'box6', 'box7', 'box8', 'box9'];
 
+const li = []
+
 const boxFactory = function(box) {
     let gameButton = document.createElement('button');
-    gameButton.textContent = '';
+    gameButton.textContent = 'X';
     gameButton.classList.add('box', box);
     gameButton.setAttribute('id', box)
     const append = function() {
@@ -13,13 +16,44 @@ const boxFactory = function(box) {
     }
     return { gameButton, append };
 }
-
 for (let i = 0; i < gameBoard.length; i++) {
     gameBoard[i] = boxFactory(gameBoard[i]);
     gameBoard[i].append();
 }
 
-const gamePlay = function() {
+const gameValue = gameBoard.filter(x => {
+    li.push(x.gameButton.textContent)
+})
+if (li[0] === li[1] && li[2] || li[3] === li[4] && li[5]
+    || li[6] === li[7] && li[8] || li[0] === li[3] && li[6]
+    || li[1] === li[4] && li[7] || li[2] === li[5] && li[8]
+    || li[0] === li[4] && li[8] || li[2] === li[4] && li[6]) {
+    console.log(true)
+}
+
+const gameFlow = function() {
+    const gameEnd = function() {
+        let num = 0;
+        for (let i = 0; i < gameBoard.length; i++) {
+            if(gameBoard[i].gameButton.textContent === '') {
+                num = num + 1
+            }
+        }
+        return num
+    } 
+    console.log(gameEnd())
+    
+    if (gameEnd()) {
+        console.log('game')
+    } else {
+        console.log('over')
+    }
+    
+}
+
+
+const gamePlay = (function() {
+    let checkWinner = ''
     const players = [
         {
             name: 'Player One',
@@ -48,14 +82,15 @@ const gamePlay = function() {
 
     const playRound = (e) => {
         if(e.target.textContent) return
-        e.target.textContent = getActivePlayer().mark 
+        e.target.textContent = getActivePlayer().mark
+        checkWinner = getActivePlayer().name 
         console.log(getActivePlayer().mark);
         changePlayer();
     };
 
     return { getActivePlayer, playRound }
 
-}();
+})();
 
 container.addEventListener('click', e => {
     gamePlay.playRound(e)
